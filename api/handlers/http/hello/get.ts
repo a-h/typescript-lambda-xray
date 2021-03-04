0a3ae6a739099025998d8b40f7fc7bd68de0ffc3
Adrian Hesketh <adrianhesketh@hushmail.com>
2021-03-04 14:02:18 +0000

    feat: improve the typing

--- api/handlers/http/hello/get.ts
+++ api/handlers/http/hello/get.ts
@@ -1,31 +1,39 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from "aws-lambda";
 import { metricScope, Unit } from "aws-embedded-metrics";
 import { xrayScope } from "../../../xray";
 import axios from "axios";
 import log from "../../../logger";
 import EventBridge from "aws-sdk/clients/eventbridge";
 
-export const handler = xrayScope((segment) =>
-  metricScope(
-    (metrics) => async (_event: APIGatewayProxyEvent, _context: Context): Promise<APIGatewayProxyResult> => {
-      log.info("hello/get starting")
-      segment.addAnnotation("source", "apiHandler");
-      metrics.putMetric("count", 1, Unit.Count);
-      try {
-        await axios.get("http://httpstat.us/500");
-      } catch (err) {
-        log.warn("httpstat.us gave error, as expected", { status: 500 });
-        // {"status":500,"level":"warn","message":"httpstat.us gave error, as expected","timestamp":"2020-09-01T18:33:30.296Z"}
+export const handler = xrayScope<APIGatewayProxyEvent, APIGatewayProxyResult>(
+  (segment) =>
+    metricScope(
+      (metrics) => async (
+        _e: APIGatewayProxyEvent,
+        _c: Context
+      ): Promise<APIGatewayProxyResult> => {
+        log.info("hello/get starting");
+        segment.addAnnotation("source", "apiHandler");
+        metrics.putMetric("count", 1, Unit.Count);
+        try {
+          await axios.get("http://httpstat.us/500");
+        } catch (err) {
+          log.warn("httpstat.us gave error, as expected", { status: 500 });
+          // {"status":500,"level":"warn","message":"httpstat.us gave error, as expected","timestamp":"2020-09-01T18:33:30.296Z"}
+        }
+        await axios.get("https://jsonplaceholder.typicode.com/todos/1");
+        metrics.putMetric("exampleApiCallsMade", 2);
+        await putEvent();
+        return {
+          statusCode: 200,
+          body: "World!",
+        };
       }
-      await axios.get("https://jsonplaceholder.typicode.com/todos/1");
-      metrics.putMetric("exampleApiCallsMade", 2);
-      await putEvent();
-      return {
-        statusCode: 200,
-        body: "World!"
-      }
-    } 
-  )
+    )
 );
 
 const putEvent = async () => {
--- api/xray.ts
+++ api/xray.ts
@@ -1,16 +1,18 @@
 import * as AWSXRay from "aws-xray-sdk";
-import {  Context } from "aws-lambda";
+import { Context } from "aws-lambda";
 import { Subsegment } from "aws-xray-sdk";
 
-export type Handler<TEvent = any, TResult = any> = (
-    event: TEvent,
-    context: Context,
+export type Handler<TEvent, TResult> = (
+  event: TEvent,
+  context: Context
 ) => Promise<void | TResult>;
 
 export const xrayScope = <
   TEvent,
   TResult,
-  F extends (segment: Subsegment) => Handler<TEvent, TResult>
+  F extends (segment: Subsegment) => Handler<TEvent, TResult> = (
+    s: Subsegment
+  ) => Handler<TEvent, TResult>
 >(
   fn: F
 ): Handler<TEvent, TResult> => async (e, c) => {
@@ -18,9 +20,11 @@ export const xrayScope = <
   AWSXRay.captureHTTPsGlobal(require("http"), true);
   AWSXRay.captureHTTPsGlobal(require("https"), true);
   AWSXRay.capturePromise();
-  const segment = AWSXRay.getSegment().addNewSubsegment("handler");
+  const segment = (
+    AWSXRay.getSegment() || new AWSXRay.Segment("lambda")
+  ).addNewSubsegment("handler");
   try {
-    return await fn(segment)(e, c)
+    return await fn(segment)(e, c);
   } finally {
     if (!segment.isClosed()) {
       segment.close();
